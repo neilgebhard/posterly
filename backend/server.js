98d04c8379a24e1faa806f22ea8050202e7fa5cc
Neil Gebhard <neilgebhard@gmail.com>
2021-11-07 18:05:04 +1000

    Add correct base dir to deployment config

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,12 @@ app.use(attachUser);
 app.use("/api", authRoutes);
 app.use("/api", postRoutes);
 
+const __basedir = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__basedir, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__basedir, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
