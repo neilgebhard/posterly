a090dd815b404059bff11c14a0bc259899f4353e
Neil Gebhard <neilgebhard@gmail.com>
2022-01-08 00:24:30 +1000

    :card_file_box: Add more data to mock posts

--- backend/data/posts.js
+++ backend/data/posts.js
@@ -44,6 +44,44 @@ const posts = [
       },
     ],
   },
+  {
+    username: "BtownBrelooms",
+    title:
+      "What is something that was used heavily in the year 2000, but it's almost never used today?",
+    comments: [
+      {
+        username: "Confusionator5000",
+        text: "Any sort of dedicated music-playing device, before that just became a part of your phone.",
+      },
+      {
+        username: "FRB2992",
+        text: "Limewire",
+      },
+      {
+        username: "AdamoclesYT",
+        text: "Colored spikey hair gel",
+      },
+    ],
+  },
+  {
+    username: "Lionzxz",
+    title:
+      "[Serious] What's a scary science fact that the public knows nothing about?",
+    comments: [
+      {
+        username: "pbourree",
+        text: "If your dog swims in a lake after receiving a spot on flea treatment - it absolutely decimates the invertibrate population. A large dog swimming in 8 Olympic swimming pools worth of water soon after treatment will leech enough neurotoxin to kill 50% of the lake's invertebrate population within 48 hours. I say after I mean relatively soon after, within say a day, to have an effect quite this devistating. The leeching does reduce over the month, but it's still there and the effect of multiple dogs still allows for a terrible buildup of chemicals.",
+      },
+      {
+        username: "TheJWeed",
+        text: "I only recently learned that when you get sunburned, the burn isn’t because of skin cell damage. The UV radiation damages the DNA. Then the skin cells decide to commit suicide and fall off so that the damaged DNA doesn’t produce cancer. I’ll never be mad at my skin peeling again.",
+      },
+      {
+        username: "DonkeyTron42",
+        text: "If you're exposed to rabies and start to show symptoms, your chance of survival is virtually zero percent.",
+      },
+    ],
+  },
 ];
 
 module.exports = posts;
