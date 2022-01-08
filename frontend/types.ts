fb5c2641298bae043b548bbd83a2a49b551139c6
Neil Gebhard <neilgebhard@gmail.com>
2022-01-08 11:11:35 +1000

    :label: Add types for post, comment, reply, and user

---
+++ frontend/types.ts
@@ -0,0 +1,35 @@
+export type Post = {
+  _id: string;
+  username: string;
+  user: string;
+  title: string;
+  comments: Comment[];
+  upvotes: string[];
+  downvotes: null[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Comment = {
+  _id: string;
+  username: string;
+  user: string;
+  text: string;
+  replies: Reply[];
+};
+
+export type Reply = {
+  _id: string;
+  username: string;
+  user: string;
+  text: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type User = {
+  _id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+};
